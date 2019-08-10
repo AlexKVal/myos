360c9ae7ce2a7704b76ae0f5ae57575325011e3e
Alexander Shemetovskiy <alexkval@gmail.com>
2019-08-10 23:09:30 +0100

    Implement Bare Metal Target

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(core_intrinsics)]
+#![no_std]
+#![no_main]
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+fn panic(_info: &PanicInfo) -> ! {
+    unsafe { intrinsics::abort() }
+}
+
+// bootimage build --target=myos.json
+#[no_mangle]
+pub fn _start() -> ! {
+    loop {}
 }
