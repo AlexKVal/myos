64e9d6504396101edf9fbf768a4764add695f5fa
Alexander Shemetovskiy <alexkval@gmail.com>
2019-08-13 21:03:16 +0100

    Check this out that lines printing works

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,10 @@ fn panic(_info: &PanicInfo) -> ! {
     unsafe { intrinsics::abort() }
 }
 
-static HELLO: &[u8] = b"Oh Yeah! This is MyOS :)";
-
 // cargo xrun
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
