32ac46d190f9025a7ef77020029bf1bc57d19a02
Alexander Shemetovskiy <alexkval@gmail.com>
2019-08-13 20:59:42 +0100

    Implement Writer#write_string

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -75,3 +75,17 @@ impl Writer {
 
     fn new_line(&mut self) {/* TODO */}
 }
+
+// VGA text buffer only supports code page 437
+impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe), // ■ character
+            }
+        }
+    }
+}
