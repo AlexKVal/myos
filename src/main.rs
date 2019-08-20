04f31ecc2f35784d1a76e8bfabd8c6f4ea60e277
Alexander Shemetovskiy <alexkval@gmail.com>
2019-08-20 22:06:57 +0100

    Implement println macro

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,7 @@ fn panic(_info: &PanicInfo) -> ! {
 // cargo xrun
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} and {}", 455, 3.1415).unwrap();
+    println!("Hello world{}", "!");
 
     loop {}
 }
